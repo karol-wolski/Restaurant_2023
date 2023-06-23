e19bfec62edbe1b5e65f1dda5ad421d2b84b308e
Karol Wolski <karolwolski04@gmail.com>
2023-06-23 21:58:13 +0200

    feat: navigation (#14)

---
+++ src/ts/mobile-menu.ts
@@ -0,0 +1,18 @@
+const menu = () => {
+  const hamburger = document.querySelector('#menu-toggle')
+  const menu = document.querySelector('#nav')
+  const menuItems = document.querySelectorAll('.nav__link')
+  hamburger.classList.toggle('hamburger--active')
+  menu.classList.toggle('nav--active')
+  menuItems.forEach(element => {
+    element.addEventListener('click', function () {
+      menu.classList.remove('nav--active')
+      hamburger.classList.remove('hamburger--active')
+    })
+  })
+}
+
+export const mobileMenu = () => {
+  const hamburger = document.querySelector('#menu-toggle')
+  hamburger.addEventListener('click', menu)
+}
