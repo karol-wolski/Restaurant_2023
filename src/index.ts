12d8b82d1162a95009ec10f228366eddbd4eca8e
Karol Wolski <karolwolski04@gmail.com>
2023-06-23 13:43:37 +0200

    feat: navigation

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import './index.css'
 import { contactForm } from './ts/contact-form'
 import { filterMenuCard, initialMenuCard } from './ts/menu-card'
+import { mobileMenu } from './ts/mobile-menu'
 
 const init = () => {
+  mobileMenu()
   initialMenuCard()
   filterMenuCard()
   contactForm()
