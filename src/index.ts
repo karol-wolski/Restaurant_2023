87076c9f04a011a0429af5b8ce2a299d113f15cc
Karol Wolski <karolwolski04@gmail.com>
2023-06-27 19:05:44 +0200

    feat: add venobox to food gallery (#21)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import './index.css'
 import 'aos/dist/aos.css'
 import AOS from 'aos'
+import 'venobox/dist/venobox.min.css'
+import VenoBox from 'venobox'
 import { BookTableForm, toggleBookTableModal } from './ts/book-table'
 import { contactForm } from './ts/contact-form'
 import { filterMenuCard, initialMenuCard } from './ts/menu-card'
@@ -16,6 +18,10 @@ const init = () => {
 
   AOS.init({
     once: false,
+  })
+  new VenoBox({
+    selector: '.venobox',
+    spinner: 'flow',
   })
 }
 
